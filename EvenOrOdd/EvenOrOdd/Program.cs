061a257ef5a82fce99a81862afd86cd3b758195d
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2023-08-17 11:46:02 -0700

    odds or even

---
+++ EvenOrOdd/EvenOrOdd/Program.cs
@@ -0,0 +1,31 @@
+﻿
+var quit = false;
+
+while (!quit)
+{
+    Console.Write("Enter an integer: ");
+    var answer = Console.ReadLine();
+    Console.WriteLine($"Answer is {answer}");
+    var number = Convert.ToInt32(answer);
+
+    string evenOrOdd;
+
+    if  (number % 2 == 0)
+    {
+        evenOrOdd = "EVEN";
+    } else
+    {
+        evenOrOdd = "ODD";
+    }
+
+    Console.WriteLine($"The number {number} is {evenOrOdd}");
+
+    Console.Write("Do you want to quit? (y/N): ");
+    var quitAnswer = Console.ReadLine();
+    if( quitAnswer == "Y" || quitAnswer == "y")
+        {
+            quit = true;
+        }
+}
+
+Console.WriteLine("Done...");
