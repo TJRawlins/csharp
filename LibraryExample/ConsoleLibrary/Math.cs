5870472c33360cc0a0775c6e79b1ae82d4eaa671
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2023-08-23 09:30:42 -0700

    update library example

---
+++ LibraryExample/ConsoleLibrary/Math.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleLibrary
+{
+    public class Math
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Mult(int x, int y)
+        {
+            return x * y;
+        }
+        public static int Div(int x, int y)
+        {
+            return x / y;
+        }
+
+    }
+}
---
+++ LibraryExample/LibraryExample/Program.cs
@@ -0,0 +1,9 @@
+﻿using clib = ConsoleLibrary;
+
+var x = 12;
+var y = 3;
+
+Console.WriteLine($"{x} + {y} = {clib.Math.Add(x,y)}");
+Console.WriteLine($"{x} - {y} = {clib.Math.Sub(x, y)}");
+Console.WriteLine($"{x} * {y} = {clib.Math.Mult(x, y)}");
+Console.WriteLine($"{x} / {y} = {clib.Math.Div(x, y)}");
