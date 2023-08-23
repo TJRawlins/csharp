ea1d955cd737fcdb0b8151e0b6dc79a424350221
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2023-08-23 11:15:53 -0700

    math library update

--- LibraryExample/ConsoleLibrary/Math.cs
+++ LibraryExample/ConsoleLibrary/Math.cs
@@ -18,6 +18,23 @@
         {
             return x / y;
         }
-
+        public static double Inverse(double x)
+        {
+            if(x == 0) throw new DivideByZeroException("x cannot be zeor");
+            return  1 / x;
+        }
+        public static int Modulo(int x, int y)
+        {
+            return x - ((x / y) * y);
+        }
+        public static int Power(int x, int y)
+        {
+            var pow = 1;
+            for (int i = 1; i <= y; i++)
+            {
+                pow *= x;
+            }
+            return pow;
+        }
     }
 }
--- LibraryExample/LibraryExample/Program.cs
+++ LibraryExample/LibraryExample/Program.cs
@@ -1,9 +1,11 @@
 ﻿using clib = ConsoleLibrary;
 
 var x = 12;
-var y = 3;
+var y = 4;
 
 Console.WriteLine($"{x} + {y} = {clib.Math.Add(x,y)}");
 Console.WriteLine($"{x} - {y} = {clib.Math.Sub(x, y)}");
 Console.WriteLine($"{x} * {y} = {clib.Math.Mult(x, y)}");
 Console.WriteLine($"{x} / {y} = {clib.Math.Div(x, y)}");
+Console.WriteLine($"{x} % {y} = {clib.Math.Modulo(x, y)}");
+Console.WriteLine($"{x} pow {y} = {clib.Math.Power(x, y)}");
