05cbf8de8abfc1c64f49d7759600e2785b58dde2
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2023-08-23 09:06:52 -0700

    added calc app

---
+++ CalculatorProject/CalculatorProject/Program.cs
@@ -0,0 +1,82 @@
+﻿
+var stack  = new Stack<double>();
+
+// user entry
+var entry = string.Empty;
+var answer = string.Empty;
+//Console.Write("Enter a number, operator, C to clear or X to quit: ");
+Console.Write("Enter the RPN statement: ");
+answer = Console.ReadLine();
+
+var operands = answer.Split(",");
+
+foreach (var op in operands)
+{
+    double num;
+    // returns boolean if it can parse
+    var isNumber = double.TryParse(op, out num);
+    if (isNumber)
+    {
+        stack.Push(num);
+        continue;
+    }
+    var num1 = stack.Pop();
+    var num2 = stack.Pop();
+    var result = op switch
+    {
+        "+" => num2 + num1,
+        "-" => num2 - num1,
+        "*" => num2 * num1,
+        "/" => num2 / num1,
+        "^" => Math.Pow(num2,num1)
+    };
+    stack.Push(result);
+}
+var finalResult = stack.Pop();
+Console.WriteLine(finalResult);
+
+/*
+do
+{
+    if (answer.ToLower() == "c")
+    {
+        stack.Clear();
+        Console.WriteLine("Cleared");
+    }
+    else if(answer == "+")
+    {
+        int num1 = stack.Pop();
+        int num2 = stack.Pop();
+        Console.WriteLine(num1 + num2);
+        stack.Push(num1 + num2);
+    }
+    else if (answer == "-")
+    {
+        int num1 = stack.Pop();
+        int num2 = stack.Pop();
+        Console.WriteLine(num2 - num1);
+        stack.Push(num2 - num1);
+    }
+    else if (answer == "*")
+    {
+        int num1 = stack.Pop();
+        int num2 = stack.Pop();
+        Console.WriteLine(num1 * num2);
+        stack.Push(num1 * num2);
+    }
+    else if (answer == "/")
+    {
+        int num1 = stack.Pop();
+        int num2 = stack.Pop();
+        Console.WriteLine(num2 / num1);
+        stack.Push(num2 / num1);
+    }
+    else
+    {
+        int num = Convert.ToInt32(answer);
+        stack.Push(num);
+    }
+    Console.Write("Enter a number, operator, C to clear or X to quit: ");
+    answer = Console.ReadLine();
+} while (answer.ToLower() != "x");
+*/
--- CollectionExample/CollectionExample/Program.cs
+++ CollectionExample/CollectionExample/Program.cs
@@ -1,5 +1,7 @@
-﻿
+﻿using System.Threading.Channels;
+
 // List
+/*
 var names = new List<string>();
 
 names.Add("Tony");
@@ -15,17 +17,104 @@ foreach (var name in names)
 {
     Console.Write($"{name} ");
 }
-
 Console.WriteLine();
+*/
 
 
-// SortedList
+
+// SORTED LIST
+/*
 var names2 = new SortedList<string,int>();
 names2.Add("Tony", 39);
 names2.Add("Tom", 20);
 names2.Add("Jane", 45);
 
-foreach (KeyValuePair<string, int> name in names2)
+foreach (KeyValuePair<string,int> name in names2)
 {
     Console.WriteLine($"Name: {name.Key}, Age: {name.Value} ");
 }
+*/
+
+
+/*
+var frames = new SortedList<int,int>();
+foreach (var frame in frames)
+{
+    var score = Console.Read();
+
+}
+*/
+
+
+
+// DICTIONARY
+/*
+var dictionary = new Dictionary<int, string>();
+dictionary.Add(1, "Red");
+dictionary.Add(2, "White");
+dictionary.Add(3, "Blue");
+dictionary.Add(4, "Orange");
+Console.WriteLine($"The color for 4 is {dictionary[4]}");
+*/
+/*
+// FIRST WAY - get occurances of each number
+var numbers = "6846846585443848850076817997663322135798778963311141718552227894";
+// dictionary preload
+var numberDictionary = new Dictionary<string, int>
+{
+    ["0"] = 0,
+    ["1"] = 0,
+    ["2"] = 0,
+    ["3"] = 0,
+    ["4"] = 0,
+    ["5"] = 0,
+    ["6"] = 0,
+    ["7"] = 0,
+    ["8"] = 0,
+    ["9"] = 0
+};
+// tunrs numbers string into an array of characters
+foreach (var ch in numbers.ToCharArray())
+{
+    // return value for each character and increments it to get the next value
+    numberDictionary[ch.ToString()]++;
+}
+foreach(var key in numberDictionary.Keys)
+{
+    Console.WriteLine($"For key {key} occurs {numberDictionary[key]} times.");
+}
+*/
+/*
+// SECOND WAY - get occurances of each number
+var numbers = "68468465854438488576817997663322135798778963311141718552227894";
+// dictionary preload
+var numberDictionary = new Dictionary<string, int>();
+// tunrs numbers string into an array of characters
+foreach (var ch in numbers.ToCharArray())
+{
+    // determine if the key isn't in the dictionary, if not, then add it
+    if (!numberDictionary.ContainsKey(ch.ToString()))
+    {
+        numberDictionary[ch.ToString()] = 0;
+    }
+    // return value for each character and increments it to get the next value
+    numberDictionary[ch.ToString()]++;
+}
+foreach (var key in numberDictionary.Keys)
+{
+    Console.WriteLine($"For key {key} occurs {numberDictionary[key]} times.");
+}
+*/
+
+
+/*
+var aList = new List<int>();
+// added 4 slots
+aList.Add(1);
+// add a range
+aList.AddRange(new int[] {1,2,3,4,5 });
+Console.WriteLine($"Default capacity is {aList.Capacity}");
+Console.WriteLine($"Default capacity is {aList.Count}");
+*/
+
+
