8b72ff86f6a10db5fb2128f3c67561e07956673f
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2023-08-23 05:55:33 -0700

    adding collections

---
+++ CollectionExample/CollectionExample/Program.cs
@@ -0,0 +1,31 @@
+﻿
+// List
+var names = new List<string>();
+
+names.Add("Tony");
+names.Add("Megan");
+names.Add("Grant");
+names.Add("Drew");
+names.Add("David");
+names.Add("Josh");
+
+names.Remove("Josh");
+
+foreach (var name in names)
+{
+    Console.Write($"{name} ");
+}
+
+Console.WriteLine();
+
+
+// SortedList
+var names2 = new SortedList<string,int>();
+names2.Add("Tony", 39);
+names2.Add("Tom", 20);
+names2.Add("Jane", 45);
+
+foreach (KeyValuePair<string, int> name in names2)
+{
+    Console.WriteLine($"Name: {name.Key}, Age: {name.Value} ");
+}
