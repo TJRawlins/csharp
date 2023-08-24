a7a577c5b0dc1de60e65c6610325050259f454a5
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2023-08-23 21:51:46 -0700

    library example mod

---
+++ ExtensionLibrary/ExtensionLibrary/StringExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace ExtensionLibrary
+{
+    public static class StringExtensions
+    {
+        // "this" makes it call on the string method
+        public static decimal ToDecimal(this string str)
+        {
+            var s = str;
+            s = s.Trim();
+            if (s[0] == '$')
+            {
+                s = s.Substring(1);
+            }
+            s = s.Replace(",", "");
+            decimal nbr;
+            var rc = decimal.TryParse(s, out nbr);
+            if (!rc)
+            {
+                throw new Exception("string contains non-numeric characters");
+            }
+            return nbr;
+        }
+        public static string AllCaps(this string str)
+        {
+            return str.ToUpper();
+        }
+        public static string AllSmall(this string str)
+        {
+            return str.ToLower();
+        }
+ 
+    }
+}
---
+++ ExtensionLibrary/TestExtensionLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExtensionLibrary;
+
+/*
+var s = "agsKKSDFIJGHasjdf";
+var sAllCaps = s.AllCaps();
+var sAllSmall = sAllCaps.AllSmall();
+Console.WriteLine($"{s} as all caps is {sAllCaps} and all small is {sAllSmall}");
+*/
+
+var s = "   $123,456.78";
+Console.WriteLine($"{s} as a decimal is {s.ToDecimal():C}");
